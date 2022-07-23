104bb304969db8cb110e2c945df594488f2bc2e7
Karan Pandya <kpandya@vassar.edu>
2022-07-23 00:13:39 -0400

    First test on GUI Unsuccessful

---
+++ src/com/chess/engine/pieces/Knight.java
@@ -0,0 +1,81 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static com.chess.engine.board.Move.*;
+
+public class Knight extends Piece {
+    //These are the 8 possible locations that the knight could move to
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {-17, -15, -10, -6, 6, 10, 15, 17};
+
+    public Knight(final Alliance pieceAlliance, final int piecePosition) {
+        super(PieceType.KNIGHT,piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(final Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES){
+
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset; //This applies the math needed to check the possible positions
+
+            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+
+                //The math breaks down if the horse is on certain columns so this catches all of those exceptions
+                if (isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                        isSecondColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                        isSeventhColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                        isEighthColumnExclusion(this.piecePosition, currentCandidateOffset)) {
+                    continue;
+                }
+
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                if (!candidateDestinationTile.isTileOccupied()){
+                    legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                } else{
+                    final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                    final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                    if (this.pieceAlliance != pieceAlliance) {
+                        legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination)); //If the occupied spot is not your piece you can take it and therefore is a legal move
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    @Override
+    public Knight movePiece(Move move) {
+        return new Knight(move.getMovedPiece().getPieceAlliance(), move.getDestinationCoordinate());
+    }
+
+    @Override
+    public String toString(){
+        return PieceType.KNIGHT.toString();
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -17 || candidateOffset == -10 || candidateOffset == 6 || candidateOffset == 15);
+    }
+    private static boolean isSecondColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.SECOND_COLUMN[currentPosition] && (candidateOffset == -10 || candidateOffset == 6);
+    }
+    private static boolean isSeventhColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.SEVENTH_COLUMN[currentPosition] && (candidateOffset == -6 || candidateOffset == 10);
+    }
+    private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -15 || candidateOffset == -6 || candidateOffset == 10 || candidateOffset == 17);
+    }
+}
---
+++ src/com/chess/engine/player/BlackPlayer.java
@@ -0,0 +1,73 @@
+package com.chess.engine.player;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.chess.engine.pieces.Piece;
+import com.chess.engine.pieces.Rook;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class BlackPlayer extends Player {
+    public BlackPlayer(final Board board, final Collection<Move> whiteStandardLegalMoves, final Collection<Move> blackStandardLegalMoves) {
+        super(board,blackStandardLegalMoves, whiteStandardLegalMoves); //In the parent class the second input is a list of a legal moves, so for black we check the black standard legal moves
+    }
+
+    @Override
+    public Collection<Piece> getActivePieces() {
+        return this.board.getBlackPieces();
+    }
+
+    @Override
+    public Alliance getAlliance() {
+        return Alliance.BLACK;
+    }
+
+    @Override
+    public Player getOpponent() {
+        return this.board.whitePlayer();
+    }
+
+    @Override
+    protected Collection<Move> calculateKingCastles(final Collection<Move> playerLegals, final Collection<Move> opponentsLegals) {
+
+        final List<Move> kingCastles = new ArrayList<>();
+
+        if (this.playerKing.isFirstMove() && !this.isInCheck()){
+            //Blacks King Side Castle
+            if (!this.board.getTile(5).isTileOccupied() && !this.board.getTile(6).isTileOccupied()){
+                final Tile rookTile = this.board.getTile(7);
+
+                if (rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()){
+                    if (Player.calculateAttacksOnTile(5, opponentsLegals).isEmpty() &&
+                            Player.calculateAttacksOnTile(6, opponentsLegals).isEmpty() &&
+                            rookTile.getPiece().getPieceType().isRook()){
+                        //TO DO A CASTLE MOVE
+                        kingCastles.add(new Move.KingSideCastleMove(this.board, this.playerKing, 6, (Rook)rookTile.getPiece(), rookTile.getTileCoordinate(), 5));
+                    }
+                }
+            }
+            //Whites Queen Side Castle
+            if (!this.board.getTile(1).isTileOccupied() && !this.board.getTile(2).isTileOccupied() && !this.board.getTile(3).isTileOccupied()){
+                final Tile rookTile = this.board.getTile(0);
+
+                if (rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()){
+                    if (Player.calculateAttacksOnTile(1, opponentsLegals).isEmpty() &&
+                            Player.calculateAttacksOnTile(2, opponentsLegals).isEmpty() &&
+                            Player.calculateAttacksOnTile(3, opponentsLegals).isEmpty() &&
+                            rookTile.getPiece().getPieceType().isRook()){
+                        //TO DO A CASTLE MOVE
+                        kingCastles.add(new Move.QueenSideCastleMove(this.board, this.playerKing, 2, (Rook)rookTile.getPiece(), rookTile.getTileCoordinate(), 3));
+                    }
+                }
+            }
+        }
+
+
+        return ImmutableList.copyOf(kingCastles);
+    }
+}
